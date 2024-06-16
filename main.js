002d1b54fa58ca1288784efa3f2cb697d9251da3
andrishe <andrishelhyan@gmail.com>
2024-06-16 21:13:59 +0200

    secondfgenerator

--- main.js
+++ main.js
@@ -1,26 +1,45 @@
-let qrcode;
+import * as QRCode from "qrcode";
 
-const generateQRCode = () => {
-  if (document.querySelector("input").value === "" || document.querySelector("input").value === " ") {
-    alert("Please enter a valid URL")
-  }
-  else { 
-    qrcode = new QRCode(document.querySelector(".qrcode"));
-    qrcode.makeCode(document.querySelector("input").value)
-  }
-}
+document.addEventListener("DOMContentLoaded", function () {
+  const inputElement = document.querySelector(".input");
+  const qrcodeContainer = document.querySelector(".qrcode");
+  const downloadButton = document.querySelector(".download-button");
 
+  const generateQRCode = async () => {
+    const input = inputElement.value.trim();
 
-const downloadQRCode = () => {
-  let img = document.querySelector(".qrcode canvas").toDataURL("image/png");
+    if (!input) {
+      alert("Please enter a valid URL");
+      return;
+    }
 
+    // Clear any previous QR code
+    qrcodeContainer.innerHTML = "";
 
-  let link = document.createElement('a');
-  link.download = 'QRCode.png';
-  link.href = img;
+    // Generate a new QR code
+    const qrCodeImage = await QRCode.toDataURL(input, { width: 128 });
+    const imgElement = document.createElement("img");
+    imgElement.src = qrCodeImage;
+    qrcodeContainer.appendChild(imgElement);
+  };
 
-  link.click();
-}
+  const downloadQRCode = () => {
+    const imgElement = qrcodeContainer.querySelector("img");
 
+    if (!imgElement) {
+      alert("Please generate a QR Code first");
+      return;
+    }
 
-document.querySelector('.download-button').addEventListener('click', downloadQRCode)
+    const link = document.createElement("a");
+    link.download = "QR.png";
+    link.href = imgElement.src;
+    link.click();
+  };
+
+  // Attach event to the input field
+  inputElement.addEventListener("input", generateQRCode);
+
+  // Attach click event to the download button
+  downloadButton.addEventListener("click", downloadQRCode);
+});
